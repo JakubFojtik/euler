19e68a70a3ec835a5f12ba7dc98395fe45a6cd1a
Jakub Fojtík <jakub.fojtik@email.cz>
2019-09-12 16:27:00 +0200

    euler1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            E1Sum();
+        }
+
+        private static void E1Sum()
+        {
+            {
+                //Brute force
+                var numbers = Enumerable.Range(1, 1000);
+                var sum = (from k in numbers where k % 3 == 0 || k % 5 == 0 select k)
+                    .Sum();
+                Console.WriteLine(sum);
+            }
+            {
+                //Constant-time
+                var limit = 1000;
+                int sumTo(int n) => n * (n + 1) / 2;
+                int sumDivisors(int n, int div) => sumTo(n / div) * div;
+                var sum = sumDivisors(limit, 3) + sumDivisors(limit, 5) - sumDivisors(limit, 15);
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
